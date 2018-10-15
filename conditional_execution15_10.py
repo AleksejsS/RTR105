ae952ed7ed3fbd166c0449f8f7ef4861a6751133
user <aleksejs.saraps.inbox.lv>
2018-10-15 15:50:27 +0300

    20181015_15_50

---
+++ conditional_execution15_10.py
@@ -0,0 +1,94 @@
+
+
+'''
+x = 5
+if x < 10:
+    print("smaller")
+if x > 20:
+    print("bigger")
+print("finish")
+'''
+'''
+x = 5
+if x == 5:
+    print("equals 5")
+if x > 4:
+    print("greater than 4")
+if x >= 5:
+    print("greater than or equals 5")
+if x < 6:
+    print("less than 6")
+if x <= 5:
+    print("less than or equals 5")
+if x != 6:
+    print("not equal 6")
+'''
+'''
+x = 5
+print("before 5")
+if x ==5:
+    print("is 5")
+    print("is still")
+    print("third 5")
+print("afterwards 5")
+print("before 6")
+if x == 6:
+    print("is 6")
+    print("is still 6")
+    print("third 6")
+print("afterwards")
+'''
+'''
+x = 152
+if x > 1:
+    print("more then one")
+if x < 100:
+    print("less than 100")
+print("all meow")
+'''
+'''
+x = 4
+if x > 2:
+    print("bigger")
+else:
+        pint("smaller")
+
+print("all meow")
+'''
+
+x = 5
+if x < 2:
+    print("small")
+elif x < 10:
+    print("Medium")
+else:
+    print("LARGE")
+
+print("all meow")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
